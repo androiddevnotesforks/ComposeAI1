501ee6bc01452bfa1336979ee4629d5a0ef75860
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2025-10-13 18:16:57 +0200

    🔖 Version 1.9.0 - 32

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ebfstudio.appgpt"
         minSdk = (findProperty("android.minSdk") as String).toInt()
         targetSdk = (findProperty("android.targetSdk") as String).toInt()
-        versionCode = 31
-        versionName = "1.8.0"
+        versionCode = 32
+        versionName = "1.9.0"
     }
 
     buildFeatures {
